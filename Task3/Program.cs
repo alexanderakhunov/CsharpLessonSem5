484debc7e5b0873c88b36780fbf5f9fa0770d05c
AlexanderAkh <sancheek240@mail.ru>
2023-02-02 17:44:21 +0300

    add task2,3

---
+++ Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задать массив и проверить присутствует ли число
+
+int[] arrayFill(int size, int leftr, int rightr)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(leftr, rightr + 1);
+    }
+    return array;
+}
+
+void PrintArr(int [] array)
+{
+    System.Console.WriteLine($"[" + String.Join(", ", array) + "]");
+}
+
+int inputNumb()
+{
+    System.Console.WriteLine("Введите число: ");
+    int numb = Convert.ToInt32(Console.ReadLine());
+    return numb;
+}
+
+bool numbCheck(int numb, int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (numb == array[i])
+        {
+            return true;
+        }
+           
+    }
+     return false;
+}
+
+
+int[] arr = arrayFill(10, -5, 5);
+PrintArr(arr);
+int number = inputNumb();
+
+if(numbCheck(number, arr))
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
